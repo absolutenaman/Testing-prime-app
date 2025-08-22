0a0c4711964367923640ec3f46a3c25371f888e5
absolutenaman <absolute.naman@gmail.com>
2025-08-23 00:01:53 +0530

    Added methods to read user input and checks whether the number is prime or not and added tests for every function

--- main.go
+++ main.go
@@ -1,5 +1,82 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	//print a welcome message
+	intro()
+	//create a channel to indicate when the user wants to quit
+	doneChan := make(chan bool)
+	//start a goroutine to read the user's input and and run the prime function
+	go readUserData(os.Stdin, doneChan)
+	//block until the done channel gets a value
+	<-doneChan
+	//close the channel
+	//say Goodbye
+	GoodBye()
+}
+func intro() {
+	fmt.Println("Hello User , It's nice to finally see you")
+	fmt.Println("Instructions !!!")
+	fmt.Println("Please enter a number to check whether it's prime or not, to quit press q")
+	prompt()
+}
+func prompt() {
+	fmt.Printf("->")
+}
+func readUserData(in io.Reader, doneChan chan bool) {
+	scanner := bufio.NewScanner(in)
+	for {
+		res, done := checkNumbers(scanner)
+		if done {
+			doneChan <- true
+			return
+		}
+		fmt.Println(res)
+		prompt()
+	}
+}
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// read user input
+	scanner.Scan()
+
+	// check to see if the user wants to quit
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// try to convert what the user typed into an int
+	numToCheck, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		return "Please enter a whole number!", false
+	}
+
+	_, msg := isPrime(int(numToCheck))
+
+	return msg, false
+}
+
+func GoodBye() {
+	fmt.Println("Thanks for visiting")
+}
+func isPrime(n int) (bool, string) {
+	if n == 1 || n == 0 {
+		return false, fmt.Sprintf("0 and 1 are not prime numbers")
+	}
+	if n < 0 {
+		return false, fmt.Sprintf("Negative numbers are not prime numbers")
+	}
+	for i := 2; i <= n/2; i++ {
+		if n%i == 0 {
+			return false, fmt.Sprintf("The number entered %d is not a prime number as it got divided by %d", n, i)
+		}
+	}
+	return true, fmt.Sprintf("The number entered %d is indeed a prime number", n)
 }
---
+++ main__test.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+type testForPrime struct {
+	name     string
+	msg      string
+	expected bool
+	testNum  int
+}
+
+func Test__isPrime(t *testing.T) {
+	var tests [4]testForPrime
+	tests[0] = testForPrime{name: "IsPrime", msg: "The number entered 7 is indeed a prime number", testNum: 7, expected: true}
+	tests[1] = testForPrime{name: "isNotPrime", msg: "0 and 1 are not prime numbers", testNum: 1, expected: false}
+	tests[2] = testForPrime{name: "isNotPrime", msg: "Negative numbers are not prime numbers", testNum: -1, expected: false}
+	tests[3] = testForPrime{name: "isNotPrime", msg: "The number entered 20 is not a prime number as it got divided by 2", testNum: 20, expected: false}
+
+	for i := 0; i < len(tests); i++ {
+		res, msg := isPrime(tests[i].testNum)
+		if !res && tests[i].expected {
+			t.Errorf("Expected %v but got %v", tests[i].expected, res)
+		}
+		if res && !tests[i].expected {
+			t.Errorf("Expected %v but got %v", tests[i].expected, res)
+		}
+		if msg != tests[i].msg {
+			t.Errorf("Expected message to be %s but got %s", tests[i].msg, msg)
+		}
+	}
+}
+func Test__prompt(t *testing.T) {
+
+	// Save the original stdout (console)
+	oldStdout := os.Stdout
+
+	// Create a pipe (a secret tunnel)
+	r, w, _ := os.Pipe()
+
+	// Redirect all prints to the pipe
+	os.Stdout = w
+
+	// Call the function (it prints, but we catch it in the pipe)
+	prompt()
+
+	// Close writer so we can read what was written
+	_ = w.Close()
+
+	// Restore real console again
+	os.Stdout = oldStdout
+
+	// Read the captured output
+	out, _ := io.ReadAll(r)
+
+	// Show what was captured
+	fmt.Println("Captured output:", string(out))
+
+	if string(out) != "->" {
+		t.Errorf("expected -> but got %s", string(out))
+	}
+
+}
+
+func Test__intro(t *testing.T) {
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	intro()
+	_ = w.Close()
+	os.Stdout = oldStdout
+	out, _ := io.ReadAll(r)
+	if !strings.Contains(string(out), "Hello User , It's nice to finally see you") {
+		t.Errorf("expected `Hello User , It's nice to finally see you` but got %s", string(out))
+	}
+}
+
+func Test__GoodBye(t *testing.T) {
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	GoodBye()
+	_ = w.Close()
+	os.Stdout = oldStdout
+	out, _ := io.ReadAll(r)
+	if !strings.Contains(string(out), "Thanks for visiting") {
+		t.Errorf("expected `Thanks for visiting` but got %s", string(out))
+	}
+}
+
+func Test__checkNumbers(t *testing.T) {
+	testData := []struct {
+		name  string
+		msg   string
+		input string
+		res   bool
+	}{
+		{name: "isPrime", msg: "The number entered 7 is indeed a prime number", input: "7", res: true},
+		{name: "isNotPrime", msg: "The number entered 20 is not a prime number as it got divided by 2", input: "20", res: false},
+		{name: "QUIT", msg: "", input: "q", res: false},
+		{name: "isNotNumber", msg: "Please enter a whole number!", input: "hgjh", res: false},
+	}
+
+	for i := 0; i < len(testData); i++ {
+		input := strings.NewReader(testData[i].input)
+		reader := bufio.NewScanner(input)
+		msg, res := checkNumbers(reader)
+		if res != testData[i].res && msg != testData[i].msg {
+			t.Errorf("Expected %s but got %s", testData[i].msg, msg)
+		}
+	}
+}
+func Test__readUserData(t *testing.T) {
+	done := make(chan bool)
+	var stdin bytes.Buffer
+	stdin.Write([]byte("1\nq\n"))
+	go readUserData(&stdin, done)
+	<-done
+}
